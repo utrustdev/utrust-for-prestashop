de1ee6bbc5ed3c58c724db1bb796aa221ba0b098
Daniel Coelho <danielmartinscoelho@gmail.com>
2020-07-22 09:48:45 +0100

    v1.0.0-beta1

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
-# utrust-for-prestashop
-Utrust cryptocurrencies payment gateway module for Prestashop 1.6
+![Utrust integrations](https://user-images.githubusercontent.com/1558992/67495646-1e356b00-f673-11e9-8854-1beac877c586.png)
+
+# Utrust for PrestaShop
+
+**Demo Store:** https://prestashop.store.utrust.com/
+
+Accept Bitcoin, Ethereum, Utrust Token and other cryptocurrencies directly on your store with the Utrust payment gateway for WooCommerce.
+Utrust is cryptocurrency agnostic and provides fiat settlements.
+The Utrust plugin extends WooCommerce allowing you to take cryptocurrency payments directly on your store via the Utrust API.
+Find out more about Utrust at [utrust.com](https://utrust.com).
+
+## Requirements
+
+- Utrust Merchant account
+- Online store using PrestaShop v1.6 (this module doesn't work on 1.7)
+
+## Install and Update
+
+### Installing
+
+1. Download our latest release zip file on the [releases page](https://github.com/utrustdev/utrust-for-prestashop/releases).
+2. Go to your PrestaShop admin dashboard (it should be something like https://<your-store.com>/admin).
+3. Navigate to the _Modules and Services_ -> _Modules and Services_.
+4. Click _Add a new module_ on top and upload the zip file.
+5. After the module being uploaded, click _Install_ and _Proceed with Installation_
+6. Once installed, follow the Setup instructions bellow.
+
+### Updating
+
+You can always check our [releases page](https://github.com/utrustdev/utrust-for-prestashop/releases) for a new version. To update just follow steps 2, 3 and 4 on the above install instructions.
+
+## Setup
+
+### On the Utrust side
+
+1. Go to [Utrust merchant dashboard](https://merchants.utrust.com).
+2. Log in or sign up if you didn't yet.
+3. On the left sidebar choose _Organization_.
+4. Click the button _Generate Credentials_.
+5. You will see now your `Api Key` and `Webhook Secret`, save them somewhere safe temporarily.
+
+   :warning: You will only be able to see the `Webhook Secret` once, after refreshing or changing page it will be no longer available to copy; if needed, you can always generate new credentials.
+
+   :no_entry_sign: Don't share your credentials with anyone. They can use it to place orders **on your behalf**.
+
+### On the PrestaShop side
+
+1. Go to your PrestaShop admin dashboard.
+2. Navigate to the _Modules and Services_ -> _Modules and Services_.
+3. Search for _Utrust_ in the list and click _Configure_
+4. Add your `Api Key` and `Webhook Secret` and click Save.
+
+## Features
+
+:sparkles: These are the features already implemented and planned for the Utrust for PrestaShop plugin:
+
+- [x] Creates Order and redirects to Utrust payment widget
+- [x] Receives and handles webhook payment received
+- [x] Receives and handles webhook payment cancelled
+- [ ] Starts automatic refund on Utrust when refund initiated in PrestaShop
+
+## Support
+
+Feel free to reach [by opening an issue on GitHub](https://github.com/utrustdev/utrust-for-prestashop/issues/new) if you need any help with the Utrust for PrestaShop plugin.
+
+If you're having specific problems with your account, then please contact support@utrust.com.
+
+In both cases, our team will be happy to help :purple_heart:.
+
+## Contribute
+
+This plugin was initially written by a third-party contractor ([Bluejour](https://www.bleujour.com/)/[Infopolis](https://www.infopolis.fr/)), and is now maintained by the Utrust development team.
+
+We have now opened it to the world so that the community using this plugin may have the chance of shaping its development.
+
+You can contribute by simply letting us know your suggestions or any problems that you find [by opening an issue on GitHub](https://github.com/utrustdev/utrust-for-prestashop/issues/new).
+
+You can also fork the repository on GitHub and open a pull request for the `master` branch with your missing features and/or bug fixes.
+Please make sure the new code follows the same style and conventions as already written code.
+Our team is eager to welcome new contributors into the mix :blush:.
+
+### Development
+
+If you want to get your hands dirty and make your own changes to the Utrust for PrestaShop plugin, we recommend you to install it in a local PrestaShop store (either directly on your computer or using a virtual host) so you can make the changes in a controlled environment.
+Alternatively, you can also do it in a PrestaShop online store that you have for testing/staging.
+
+Once the plugin is installed in your store, the source code should be in `modules/UTRUST`. All the changes there should be reflected live in the store.
+When you add a translation string like `l s='This is phrase' mod='UTRUST'` it should automatically appear in "Admin dashboard" -> "Localization" -> "Translations" -> "Modify Translations" (section) and choose the Type of translation "Installed modules translations" and the desired language. Search for Utrust and make the changes. Once you save, it will store the translations in Utrust module code (folder `translations`).
+f something goes wrong, logs can be found in `/log`.
+
+## Publishing
+
+For now only members of the Utrust development team can publish new versions of the Utrust for PrestaShop plugin.
+
+To publish a new version, simply follow [these instructions](https://github.com/utrustdev/utrust-for-prestashop/wiki/Publishing).
+
+## License
+
+The Utrust for PrestaShop plugin is maintained with :purple_heart: by the Utrust development team, and is available to the public under the GNU GPLv3 license. Please see [LICENSE](https://github.com/utrustdev/prestashop/blob/master/LICENSE) for further details.
+
+&copy; Utrust 2019
---
+++ controllers/front/redirect.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * 2007-2019 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2019 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
+class UTRUSTRedirectModuleFrontController extends ModuleFrontController
+{
+
+    /**
+     * Do whatever you have to before redirecting the customer on the website of your payment processor.
+     */
+    public function postProcess()
+    {
+        header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
+
+        /*
+         * Oops, an error occured.
+         */
+        if (Tools::getValue('action') == 'error') {
+            return $this->displayError('An error occurred while trying to redirect the customer');
+        } else {
+
+            $api = UTRUST::$api_sandbox;
+            if (Configuration::get('UTRUST_MODE_API')) {
+                $api = UTRUST::$api_prod;
+            }
+
+            // Get Api Key
+            $api_key = Configuration::get('UTRUST_API_KEY');
+
+            $cart_id = Context::getContext()->cart->id;
+            $secure_key = Context::getContext()->customer->secure_key;
+
+            $cart = new Cart((int) $cart_id);
+            $customer = new Customer((int) $cart->id_customer);
+
+            /**
+             * Since it's an example we are validating the order right here,
+             * You should not do it this way in your own module.
+             */
+            $payment_status = Configuration::get('PS_OS_UTRUST'); // Default value for a payment that succeed.
+            $message = null; // You can add a comment directly into the order so the merchant will see it in the BO.
+
+            /**
+             * Converting cart into a valid order
+             */
+            $module_name = $this->module->displayName;
+            $currency_id = (int) Context::getContext()->currency->id;
+
+            $this->module->validateOrder($cart_id, $payment_status, $cart->getOrderTotal(), $module_name, $message, array(), $currency_id, false, $secure_key);
+
+            $order_id = Order::getOrderByCartId((int) $cart->id);
+
+            $order_data = $this->buildOrderData($order_id, $cart->id);
+
+            $path = 'stores/orders';
+
+            $curl = curl_init();
+
+            $request_string = json_encode($order_data);
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $api . $path,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => $request_string,
+                CURLOPT_HTTPHEADER => array('Content-Type: application/json', 'Content-Length: ' . strlen($request_string), "Authorization: Bearer " . $api_key),
+            ));
+
+            $response_order = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+            $response_order_json = json_decode($response_order);
+
+            if (isset($response_order_json->data->attributes->redirect_url) && isset($response_order_json->data->id)) {
+
+                $redirect_url = $response_order_json->data->attributes->redirect_url;
+                $uuid = $response_order_json->data->id;
+
+            } else {
+                return;
+            }
+
+            $datasql = array('id_order' => $order_id,
+                'UUID' => $uuid);
+
+            $sql = 'SELECT * FROM ' . _DB_PREFIX_ . 'UTRUST WHERE id_order = ' . $order_id;
+
+            if ($results = Db::getInstance()->ExecuteS($sql)) {
+
+                if (!Db::getInstance()->update('UTRUST', $datasql, 'id_order = ' . $order_id)) {
+                    return;
+                }
+
+            } else {
+
+                if (!Db::getInstance()->insert('UTRUST', $datasql)) {
+                    return;
+                }
+
+            }
+
+            if ($order_id && ($secure_key == $customer->secure_key)) {
+                return Tools::redirect($redirect_url);
+            } else {
+
+                $this->errors[] = $this->module->l('An error occured. Please contact the merchant to have more informations');
+
+                return $this->setTemplate('error.tpl');
+            }
+        }
+    }
+
+    public function buildOrderData($order_id, $cart_id)
+    {
+        $link = new Link();
+        $order = new Cart($cart_id);
+        $line_items = array();
+        $order_items = $order->getProducts(true);
+        $shipping_total = null;
+        $tax_total = null;
+
+        // Line items
+        foreach ($order_items as $product) {
+
+            $line_item = array(
+                'sku' => $product['id_product'] . '-' . $product['id_product_attribute'],
+                'name' => $product['name'] . ' - ' . $product['attributes_small'],
+                'price' => strval($product['price']),
+                'currency' => 'EUR',
+                'quantity' => $product['quantity'],
+            );
+            $line_items[] = $line_item;
+        }
+
+        $reference = $order_id;
+
+        // Order info
+        $order_data = array(
+            'reference' => $reference,
+            'amount' => array(
+                'total' => strval($order->getOrderTotal(true, 3)),
+                'currency' => 'EUR',
+                'details' => array(
+                    'subtotal' => strval($order->getOrderTotal(false)),
+                    'tax' => strval($order->getOrderTotal(true, 4) - $order->getOrderTotal(false, 4)),
+                    'shipping' => strval($order->getOrderShippingCost()),
+                    'discount' => strval($order->getOrderTotal(true, 2)),
+                ),
+            ),
+            'return_urls' => array(
+                'return_url' => $this->context->link->getModuleLink('UTRUST', 'confirmation', array('order_id' => $order_id, 'cart_id' => $cart_id, 'secure_key' => Context::getContext()->customer->secure_key)),
+                'cancel_url' => $this->context->link->getModuleLink('UTRUST', 'confirmation', array('order_id' => $order_id, 'cart_id' => $cart_id, 'secure_key' => Context::getContext()->customer->secure_key, 'cancel' => true)),
+                'callback_url' => $this->context->link->getModuleLink('UTRUST', 'validation', array()),
+            ),
+            'line_items' => $line_items,
+        );
+
+        $address = new Address($this->context->cart->id_address_invoice);
+
+        // Customer info
+        $customer = array(
+            'first_name' => $address->firstname,
+            'last_name' => $address->lastname,
+            'email' => $this->context->customer->email,
+            'address1' => $address->address1,
+            'address2' => $address->address2,
+            'city' => $address->city,
+            'state' => '',
+            'postcode' => $address->postcode,
+            'country' => $address->country,
+        );
+
+        $request = array(
+
+            'data' => array(
+                'type' => 'orders',
+                'attributes' => array(
+                    'order' => $order_data,
+                    'customer' => $customer,
+                ),
+            ),
+        );
+        return $request;
+    }
+
+    protected function displayError($message, $description = false)
+    {
+        /*
+         * Create the breadcrumb for your ModuleFrontController.
+         */
+        $this->context->smarty->assign('path', '
+			<a href="' . $this->context->link->getPageLink('order', null, null, 'step=3') . '">' . $this->module->l('Payment') . '</a>
+			<span class="navigation-pipe">&gt;</span>' . $this->module->l('Error'));
+
+        /*
+         * Set error message and description for the template.
+         */
+        array_push($this->errors, $this->module->l($message), $description);
+
+        return $this->setTemplate('error.tpl');
+    }
+}
